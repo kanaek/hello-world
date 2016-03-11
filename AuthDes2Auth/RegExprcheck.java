92ffbac04cd05ce5a583e4d43b7aa82e3211f369
kanaek <kanrun123@hotmail.com>
2016-03-11 18:08:28 +0800

    1

--- AuthDes2Auth/RegExprcheck.java
+++ AuthDes2Auth/RegExprcheck.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
  * Created by user on 2016/3/10.
  */
 public class RegExprcheck {
+    private enum judege_state {RED,BLUE,BLACK,YELLOW,GREEN  };
     public static void main(String[] args) throws Exception{
         File file = new File("C:\\git\\giy\\ownsvnpan\\src\\SIRM权限方案.authz");
         String temp = null;
@@ -18,7 +19,7 @@ public class RegExprcheck {
         BufferedReader br=new BufferedReader(read);
 
        // if (temp.equals("")) System.out.println("he"); //
-        temp = br.readLine();
+        //temp = br.readLine();
         /*
         try {
             AddDir2SVN add = new AddDir2SVN("https://user-PC/svn/qq","kr","123");
@@ -45,12 +46,55 @@ public class RegExprcheck {
             System.out.println(iterator.next());
         } */
 
-
+        HashMap<String,HashSet<String>> rolemap = new HashMap<String, HashSet<String>>();
+        int count = 0;
+        int count1 = 0;
+        int judge_state = 0; //0表示开始，1表示读入[role]，2表示读入dept=kr,hu....，3表示读入[directory],4表示读入需创建的文件夹,5表示读入[/文件夹路径],
+        //6表示读入@dept=rw，可转至5
+        boolean errorstate = false;
 
         while ((temp= br.readLine()) !=null) {
-            //System.out.println(temp);
-            if ()temp.matches("\\[groups\\]");
-            else System.out.println(temp);
+            temp = temp.replace(" ","");
+            temp = temp.replaceAll("\t","");
+            if (temp.matches("\\[role\\]") && judge_state == 0) {
+                System.out.println("role");
+
+                judge_state = 1;
+            }
+            else if (temp.matches("[a-zA-Z_]+=([a-z.0-9]+(,[a-z.0-9]+)*,?)*") && judge_state ==1 ) {
+                String[] temp2 = temp.split("=");
+                String temp3 = temp2[1];
+                String[] temp4 = temp3.split(",");
+                HashSet<String> temp5 = new HashSet<String>();
+                for (String item : temp4) {
+                     temp5.add(item);
+                }
+                rolemap.put(temp2[0],temp5);
+                System.out.println(temp2[0]);
+                System.out.println("hh");
+            }
+            else if (temp.matches("\\[directory\\]") && judge_state == 1 ) {
+                System.out.println("dir");
+                judge_state = 2;
+            }
+            else if (temp.matches("(/[a-zA-Z._0-9\u4E00-\u9FFF]+)(/[a-zA-Z._0-9\u4E00-\u9FFF]+)*/?") && judge_state == 2) {
+
+                System.out.println(count++);
+            }
+            else if (temp.matches("\\[(/[a-zA-Z._0-9\u4E00-\u9FFF]+)(/[a-zA-Z._0-9\u4E00-\u9FFF]+)*/?\\]") && (judge_state ==2 || judge_state ==4 )) {
+                System.out.println("count1:"+count1++);
+                judge_state = 3;
+            }
+            else if (temp.matches("@[a-z]+=(r|rw)*") && (judge_state == 3 || judge_state ==4) ) {
+                System.out.println("auth");
+                judge_state = 4;
+            }
+            else if (temp.equals("")){
+                //errorstate = true;
+                System.out.println("nothing");
+            } else {
+                System.out.println("error line :"+temp);
+            }
         }
 
     }
---
+++ AuthDes2Auth/State.java
@@ -0,0 +1,9 @@
+package AuthDes2Auth;
+
+/**
+ * Created by user on 2016/3/11.
+ */
+public interface State {
+    public String sayHello();
+    public String sayGoodbye();
+}
